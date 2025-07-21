b2cad7798518278f34502861d9229f4935eb5d66
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-07-21 16:24:32 -0500

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Alejandro La Rosa on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
