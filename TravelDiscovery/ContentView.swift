a7b0220dd5bba5fbab08a4a1efe59f928c1c745d
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-07-21 18:03:05 -0500

    Add: destination images and display data

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -59,7 +59,17 @@ struct DiscoverCategoriesView: View {
     }
 }
 
+struct Destination: Hashable {
+    let name, country, imageName: String
+}
+
 struct PopularDestinationsView: View {
+    let destinations: [Destination] = [
+        .init(name: "Paris", country: "France", imageName: "eiffel_tower"),
+        .init(name: "Tokyo", country: "Japan", imageName: "japan"),
+        .init(name: "New York", country: "US", imageName: "new_york")
+    ]
+    
     var body: some View {
         VStack {
             HStack {
@@ -73,9 +83,26 @@ struct PopularDestinationsView: View {
             
             ScrollView(.horizontal) {
                 HStack(spacing: 8) {
-                    ForEach(0..<5, id: \.self) { num in
-                        Spacer()
-                            .frame(width: 125, height: 150)
+                    ForEach(destinations, id: \.self) { destination in
+                        VStack(alignment: .leading, spacing: 0) {
+                            Image(destination.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 125, height: 125)
+                                .cornerRadius(5)
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 6)
+                                
+                            
+                            Text(destination.name)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                            Text(destination.country)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                                .padding(.bottom, 8)
+                                .foregroundStyle(Color.gray)
+                        }
                             .background(Color(.init(white: 0.9, alpha: 1)))
                             .cornerRadius(5)
                             .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
